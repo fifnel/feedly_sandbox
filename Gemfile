08b2611c808fcbc53497be9a2bd02f59bce022d0
fifnel <fifnel@gmail.com>
2014-04-30 21:17:06 +0900

    ProMotion

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rake'
 # Add your dependencies here:
+gem 'motion-cocoapods'
+gem 'bubble-wrap'
+
+gem 'ProMotion'
---
+++ app/screens/main_screen.rb
@@ -0,0 +1,13 @@
+class MainScreen < PM::Screen
+
+  def on_load
+    set_nav_bar_button :right, title:"Test", action: :open_test
+  end
+
+  def open_test
+    open TestScreen
+  end
+
+
+
+end
